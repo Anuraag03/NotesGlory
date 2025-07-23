89f452a611c34d1db76c3008182518d2d9e51a6d
Anuraag03 <f20211887@hyderabad.bits-pilani.ac.in>
2025-07-23 15:17:04 +0530

    Delete backend directory

--- backend/middlewares/error-middleware.js
+++
@@ -1,9 +0,0 @@
-const errorMiddleware = (err, req, res, next) => {
-  const status = err.status || 500;
-  const message =
-    err.message ||
-    "Something went wrong. Server error. Please try again later.";
-  return res.status(status).json({ message });
-};
-
-module.exports = errorMiddleware;
