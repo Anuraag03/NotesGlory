0de1200b589013070d2b13f880c52d8de85f5f43
Anuraag03 <f20211887@hyderabad.bits-pilani.ac.in>
2025-07-26 12:21:44 +0530

    Fixed dependency issues

--- backend/index.js
+++ backend/index.js
@@ -16,13 +16,7 @@ app.use(cors({
   origin: '*',
   credentials: true,
 }));
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=origins,
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
+
 app.use(express.json());
 
 app.use('/api/auth', authRoutes);
